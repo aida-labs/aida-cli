2a1cac7f86a5dfe0515a3dee26d88eb452e160d3
Bobby Meelu <kmeelu.52@gmail.com>
2019-07-17 20:39:54 -0700

    update cli

--- Formula/aida.rb
+++ Formula/aida.rb
@@ -1,7 +1,7 @@
 class Aida < Formula
   desc "Run Spark applications in the Cloud from the command line"
   homepage "https://www.aidalabs.io/docs"
-  url "https://cdn.aidalabs.info/aida-v0.0.8.tar.gz"
+  url "https://cdn.aidalabs.info/aida-v0.0.9.tar.gz"
   sha256 "84980117f877ea0cc01cd1a267e99a26c687db60044152b7e960a4227d044c24"
   # depends_on "cmake" => :build
 
